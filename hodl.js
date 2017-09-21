9d23127897aa0c56c435979e37a31964c9a652e6
Vince Vuong <vuong.vincent@gmail.com>
2017-09-20 22:04:55 -0700

    Changed decimal for prices

--- hodl.js
+++ hodl.js
@@ -5,7 +5,7 @@ $(function(){
     url: 'https://api.coinmarketcap.com/v1/ticker/bitcoin/',
     dataType: 'json',
     success: function (data) {
-      var price = data[0].price_usd;
+      var price = Number(data[0].price_usd).toFixed(2);
       var hourChanges = data[0].percent_change_1h;
       var dayChanges = data[0].percent_change_24h;
       var weekChanges = data[0].percent_change_7d;
