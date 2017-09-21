f0e05a2209e783eadc5fcdec53590a9f2bbeb5d9
Vince Vuong <vuong.vincent@gmail.com>
2017-09-20 21:59:18 -0700

    Added additional data to topbar

--- hodl.js
+++ hodl.js
@@ -6,7 +6,13 @@ $(function(){
     dataType: 'json',
     success: function (data) {
       var price = data[0].price_usd;
+      var hourChanges = data[0].percent_change_1h;
       var dayChanges = data[0].percent_change_24h;
+      var weekChanges = data[0].percent_change_7d;
+
+      
+      $('#topbar').append(`Price: ${price} || Hourly: ${hourChanges}% || 24 Hours: ${dayChanges}% || Weekly: ${weekChanges}%`);
+
       console.log(price);
       console.log(dayChanges);
     }
