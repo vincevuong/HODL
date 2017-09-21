0be23a262222b76d8a150b3796707c468b677377
hcam05 <harry@simpleplots.com>
2017-09-20 22:43:55 -0700

    added logic for coin

--- hodl.js
+++ hodl.js
@@ -7,12 +7,18 @@ $(function(){
     success: function (data) {
       var price = Number(data[0].price_usd).toFixed(2);
       var hourChanges = data[0].percent_change_1h;
-      var dayChanges = data[0].percent_change_24h;
+      var dayChanges =  data[0].percent_change_24h;
       var weekChanges = data[0].percent_change_7d;
-
       
-      $('#topbar').append(`Price: ${price} || Hourly: ${hourChanges}% || 24 Hours: ${dayChanges}% || Weekly: ${weekChanges}%`);
 
+      $('#topbar').prepend(`Price: ${price} || Hourly: ${hourChanges}% || 24 Hours: ${dayChanges}% || Weekly: ${weekChanges}%`);
+      
+      if(dayChanges > 0){
+        $('#image').append('<img src=\"images/bitcoin_ride_up.gif\"></img>');
+      }else{
+        $('#image').append('<img src=\"images/bitcoin_ride_down.gif\"></img>');
+      }
+      
       console.log(price);
       console.log(dayChanges);
     }
